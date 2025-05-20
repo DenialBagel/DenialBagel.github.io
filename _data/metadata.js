cda3f4a931f40bd7acfbaa2b6ef353172c4c4b96
Walter Augustine <walter@Walters-MacBook-Pro.local>
2025-05-19 23:50:14 -0400

    restructure. add pagination. make header dynamic. make all pages except index inherit from base layout. add local font files

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+  title: 'Denial Bagel',
+  url: 'https://denialbagel.github.io/',
+  language: 'en',
+  description: "Rachel Yu's digital garden",
+  // author: {
+  //   name: 'Your Name Here',
+  //   email: 'youremailaddress@example.com',
+  //   url: 'https://example.com/about-me/',
+  // },
+};
